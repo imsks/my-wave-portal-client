6d2fe51b07ad236331be617d2d8b2fb43dceabb0
Sachin Shukla <sachinkshuklaoo7@gmail.com>
2021-12-17 13:43:20 +0530

    deployed webapp

---
+++ build/precache-manifest.5d764c29fe8b9c417ee7703f08690685.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a1448f8213310a3b57129354cbbfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b66f008946aab17b7f1f",
+    "url": "/static/css/main.d181b677.chunk.css"
+  },
+  {
+    "revision": "48d44aca6f436eba8e7a",
+    "url": "/static/js/2.175b7e2b.chunk.js"
+  },
+  {
+    "revision": "b66f008946aab17b7f1f",
+    "url": "/static/js/main.8e30abe2.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,118 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "./App.css";
+import WavePortal from "./utils/WavePortal.json";
 
 export default function App() {
-    const wave = () => {};
+    const [currentAccount, setCurrentAccount] = useState(null);
+    const [waveCount, setWaveCount] = useState(0);
+    const [miningStatus, setMiningStatus] = useState(null);
+
+    const checkIfWalletIsConnected = async () => {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+            console.log("Make sure you have metamask installed and logged in");
+            return;
+        }
+
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+            const [account] = accounts;
+
+            setCurrentAccount(account);
+
+            const contractAddress =
+                "0xF0849c244CF1737c57A3155d7DfEc50F13398aF5";
+
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const wavePortalAddress = new ethers.Contract(
+                contractAddress,
+                WavePortal.abi,
+                signer
+            );
+
+            let count = await wavePortalAddress.getTotalWaves();
+            setWaveCount(count.toNumber());
+        } else {
+            console.log("No accounts found");
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+            if (!ethereum) {
+                alert("Get MetaMask!");
+                return;
+            }
+
+            const accounts = await ethereum.request({
+                method: "eth_requestAccounts",
+            });
+
+            const [account] = accounts;
+
+            setCurrentAccount(account);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const wave = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                alert("Get MetaMask!");
+                return;
+            }
+
+            setMiningStatus("Waving...");
+
+            const contractAddress =
+                "0xF0849c244CF1737c57A3155d7DfEc50F13398aF5";
+
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const wavePortalAddress = new ethers.Contract(
+                contractAddress,
+                WavePortal.abi,
+                signer
+            );
+
+            const waveTxn = await wavePortalAddress.wave();
+            await waveTxn.wait();
+
+            const count = await wavePortalAddress.getTotalWaves();
+            setMiningStatus("Hi, You just waved at me. Thanks!");
+            setWaveCount(count.toNumber());
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const connectWalletButton = !currentAccount && (
+        <button className="waveButton" onClick={connectWallet}>
+            Connect wallet
+        </button>
+    );
 
     return (
-        <div className="mainContainer">
+        <div className="body">
             <div className="dataContainer">
-                <div className="header">👋 Hey there!</div>
+                <div className="header">
+                    <span role="img" aria-label="wave">
+                        👋
+                    </span>{" "}
+                    Hey there!
+                </div>
 
                 <div className="bio">
                     I'm Sachin, a full stack engineer. Connect your Ethereum
@@ -18,6 +122,9 @@ export default function App() {
                 <button className="waveButton" onClick={wave}>
                     Wave at Me
                 </button>
+                {connectWalletButton}
+                <h4 className="text">Total wave count: {waveCount}</h4>
+                <h4 className="text">{miningStatus}</h4>
             </div>
         </div>
     );
